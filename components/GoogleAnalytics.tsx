10f1e2d97b21855cc0e23267726a905303bfce06
Aditya <147301021+adityajha2005@users.noreply.github.com>
2025-01-31 17:44:27 +0530

    google analytics v2

--- components/GoogleAnalytics.tsx
+++ components/GoogleAnalytics.tsx
@@ -1,27 +1,21 @@
 'use client';
 
-import Script from 'next/script'
-
 export default function GoogleAnalytics() {
-    const measurementId = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID;
-    
-    return (
-        <>
-            <Script
-                strategy="afterInteractive"
-                src={`https://www.googletagmanager.com/gtag/js?id=${measurementId}`}
-            />
-            <Script
-                id="google-analytics"
-                strategy="afterInteractive"
-            >
-                {`
-                    window.dataLayer = window.dataLayer || [];
-                    function gtag(){dataLayer.push(arguments);}
-                    gtag('js', new Date());
-                    gtag('config', '${measurementId}');
-                `}
-            </Script>
-        </>
-    )
+  const measurementId = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID;
+  
+  return (
+    <>
+      <script async src={`https://www.googletagmanager.com/gtag/js?id=${measurementId}`}></script>
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${measurementId}');
+          `,
+        }}
+      />
+    </>
+  );
 }
